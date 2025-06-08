9829eca9a798e8729eb6778bf352bb83a0901bbd
Bhashini123868 <bhashimunasinghe286@gmail.com>
2025-06-08 12:28:51 +0530

    controllers

--- src/main/java/edu/icet/controller/OrderController.java
+++ src/main/java/edu/icet/controller/OrderController.java
@@ -1,10 +1,12 @@
 package edu.icet.controller;
 
+import edu.icet.dto.OrderDTO;
 import edu.icet.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/order")
@@ -12,4 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class OrderController {
     final OrderService service;
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> placeOrder(@RequestBody OrderDTO orderDTO){
+        return ResponseEntity.ok(service.placeOrder(orderDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderDTO>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> cancelOrder(@PathVariable Long id){
+        service.cancelOrder(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/edu/icet/controller/OrderItemController.java
+++ src/main/java/edu/icet/controller/OrderItemController.java
@@ -1,10 +1,12 @@
 package edu.icet.controller;
 
+import edu.icet.dto.OrderItemDTO;
 import edu.icet.service.OrderItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/order-item")
@@ -12,4 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class OrderItemController {
     final OrderItemService service;
+
+    @PostMapping
+    public ResponseEntity<OrderItemDTO> save(@RequestBody OrderItemDTO orderItemDTO){
+        return ResponseEntity.ok(service.save(orderItemDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderItemDTO> update(@PathVariable Long id, @RequestBody OrderItemDTO orderItemDTO){
+        return ResponseEntity.ok(service.update(id, orderItemDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderItemDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderItemDTO>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
 }
